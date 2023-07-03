e957a42645cd39ad55a3ddf8a19ffe42a6c0713e
Fran Lopez <franciscolopto@gmail.com>
2023-07-03 21:07:26 +0200

    add background and logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Bai_Jamjuree } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const bai = Bai_Jamjuree({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
 
 export default function Home() {
   return (
-    <div>Vamos a empezar la landing...</div>
+    <div
+      className={`${bai.className} text-grayish-blue bg-[url('/img/bg-header-mobile.png')] md:bg-[url('/img/bg-header-desktop.png')] bg-top bg-no-repeat bg-contain min-h-screen bg-neutral-50`}
+    >
+      <main className="flex flex-col container items-center text-center gap-32 px-8 m-auto md:max-w-screen-xl ">
+        <header className="mt-32">
+          <h1>Clipboard</h1>
+          <Image src='img/logo.svg' width={125} height={125} alt='Clipboard logo'  />
+        </header>
+      </main>
+    </div>
   );
 }
