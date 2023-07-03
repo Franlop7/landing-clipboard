b34da5f7adea0c73e8ced6f8196a9b55577c590d
Fran Lopez <franciscolopto@gmail.com>
2023-07-03 21:06:42 +0200

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'strong-cyan': 'hsl(171, 66%, 44%)',
+        'light-conic': 'hsl(233, 100%, 69%)',
+        'dark-grayish-blue': 'hsl(210, 11%, 33%)',
+        'grayish-blue': 'hsl(201, 11%, 66%)',
+        'background': '#F2F2F2',
+        'background-darker': '#979797',
+      },
     },
   },
   plugins: [],
