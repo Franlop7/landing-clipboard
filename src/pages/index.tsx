a7e87310ec7cab8a2f3c99ab0e4a433f232e2c65
Fran Lopez <franciscolopto@gmail.com>
2023-07-05 21:49:31 +0200

    add section styles img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -41,7 +41,29 @@ export default function Home() {
           </div>
         </section>
 
-        <section className="flex flex-col items-center gap-8"></section>
+        <section className="flex flex-col items-center gap-8">
+          <div>
+            <h3 className="text-[32px] text-center text-dark-grayish-blue font-semibold leading-snug mb-4">
+              Keep track of your snippets
+            </h3>
+            <p>
+              Clipboard instantly stores any item you copy in the cloud, meaning
+              you can access your snippets immediately on all your devices. Our
+              Mac and iOS apps will help you organize everything.
+            </p>
+          </div>
+          <div className="flex flex-col items-center w-full gap-8 md:flex-row">
+            <div className="w-full h-full md:relative md:basis-1/2 md:h-[30rem]">
+              <Image
+                className="h-full md:object-cover md:absolute md:top-0 md:right-0 md:overflow-x-visible md:-translate-x-[12rem] lg:-translate-x-[9rem] xl:-translate-x-24 transition-all duration-75"
+                src="/img/image-computer.png"
+                width={752}
+                height={572}
+                alt="Computer illustration"
+              />
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
