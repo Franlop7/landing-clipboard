ad8bd55daeff7e70fd249472b94978b53ca3835e
Fran Lopez <franciscolopto@gmail.com>
2023-07-26 18:18:38 +0200

    add id for key

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,22 +2,27 @@ import Link from "next/link";
 
 const menuItems = [
   {
+    id: 1,
     title: "FAQs",
     href: "faqs",
   },
   {
+    id: 2,
     title: "Contact Us",
     href: "contact-us",
   },
   {
+    id: 3,
     title: "Privacy Policy",
     href: "privacy",
   },
   {
+    id: 4,
     title: "Press Kit",
     href: "press-kit",
   },
   {
+    id: 5,
     title: "Install Guide",
     href: "install-guide",
   },
@@ -26,9 +31,9 @@ const menuItems = [
 function Nav() {
   return (
     <nav className="flex flex-col flex-wrap items-center gap-6 text-lg text-dark-grayish-blue md:flex-row md:text-base md:gap-3">
-      {menuItems.map(({ title, href }) => (
+      {menuItems.map(({ id, title, href }) => (
         <Link
-          key={href}
+          key={id}
           href={href}
           className="md:w-1/4 hover:text-strong-cyan"
         >
